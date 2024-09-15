6d37b817c59b82aa1e146da80da640bbf0ba45c9
JCWoodworker <jc@rilocalwoodworks.com>
2024-09-14 21:15:24 -0400

    replaced promptSync with readlineSync. No more line re-printing bug

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NewfuckingsubappModule } from './subapps/newfuckingsubapp/newfuckingsubapp.module';
+import { TestingmyshitModule } from './subapps/testingmyshit/testingmyshit.module';
 
 @Module({
-  imports: [NewfuckingsubappModule],
+  imports: [NewfuckingsubappModule, TestingmyshitModule],
   controllers: [AppController],
   providers: [AppService],
 })
