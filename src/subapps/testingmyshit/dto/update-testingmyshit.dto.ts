9508d2ff72e4c739838f6a6da66a5349b24bcb61
JCWoodworker <jc@rilocalwoodworks.com>
2024-09-15 21:11:43 -0400

    removed test subapps

--- src/subapps/testingmyshit/dto/update-testingmyshit.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateTestingmyshitDto } from './create-testingmyshit.dto';
-
-export class UpdateTestingmyshitDto extends PartialType(CreateTestingmyshitDto) {}
--- src/subapps/testingmyshit/testingmyshit.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TestingmyshitService } from './testingmyshit.service';
-import { TestingmyshitController } from './testingmyshit.controller';
-
-@Module({
-  controllers: [TestingmyshitController],
-  providers: [TestingmyshitService],
-})
-export class TestingmyshitModule {}
