c97fc3b18eb647729bd8f6c51e9d76e266b07f1e
JCWoodworker <jc@rilocalwoodworks.com>
2024-09-20 22:05:56 -0400

    added app tests

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -4,19 +4,24 @@ import { AppService } from './app.service';
 
 describe('AppController', () => {
   let appController: AppController;
+  let appService: AppService; 
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [AppService], // Make sure AppService is provided
     }).compile();
 
     appController = app.get<AppController>(AppController);
+    appService = app.get<AppService>(AppService); // Get the AppService instance
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return the value from appService.getHello', () => {
+      const expectedResult = 'Some value from AppService'; // Replace with the actual expected value
+      jest.spyOn(appService, 'getHello').mockReturnValue(expectedResult); // Mock the AppService method
+
+      expect(appController.getHello()).toBe(expectedResult);
     });
   });
 });
